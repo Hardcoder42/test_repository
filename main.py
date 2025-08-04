7c7825702d5ec1a0fbcb86d835c85f26025b7c0a
HardCoder42 <amonbro92@gmail.com>
2025-08-04 18:51:48 +0700

    test for homework (2)

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-tested
-teste
-test
-tes
-te
-t
+days = input('Введите количество суток: ')
+hours = int(days) * 24
+min = int(hours) * 60
+sec = int(min) * 60
+
+info = f"{days} суток = {hours} часов = {min} минут = {sec} секунд"
+print(info)
